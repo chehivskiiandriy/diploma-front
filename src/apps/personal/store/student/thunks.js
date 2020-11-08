6b2e0ca9daef2ebfc589830db1f5c4df23f34287
chekhivskyiandrii <andriy12071997@gmail.com>
2020-11-08 14:11:44 +0200

    add upload students and teachers

--- src/apps/personal/store/student/thunks.js
+++ src/apps/personal/store/student/thunks.js
@@ -11,7 +11,9 @@ import {
   CREATE_STUDENT_LOADING,
   UPDATE_STUDENT_LOADING,
   DELETE_STUDENT_LOADING,
+  UPLOAD_STUDENTS_LOADING,
 } from '../../../../store/loading/constants';
+import { pureModifier } from '../../../../api/helpers';
 
 export const getStudentsAction = () => async dispatch => {
   try {
@@ -60,3 +62,17 @@ export const deleteStudentAction = id => async dispatch => {
 };
 
 export const deleteStudent = loadingThunk(DELETE_STUDENT_LOADING)(deleteStudentAction);
+
+export const uploadStudentsAction = data => async dispatch => {
+  try {
+    await api.post('/users/upload/students', data, {
+      withCommonHeaders: false,
+      dataModifier: pureModifier,
+    });
+    dispatch(getStudents());
+  } catch (e) {
+    //
+  }
+};
+
+export const uploadStudents = loadingThunk(UPLOAD_STUDENTS_LOADING)(uploadStudentsAction);
